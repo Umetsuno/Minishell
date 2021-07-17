1a51b7951cf33f07fbb83e2e353c9be1da2ed7a3
Sacha Baranes <user@iMac-de-user-5.local>
2021-07-17 18:36:17 +0200

    add commentaires

--- Sources/main.c
+++ Sources/main.c
@@ -45,7 +45,7 @@ void	recover_data(t_data *data)
 	if (ft_strchr(data->line, '>') != NULL) 
 		scan_fd(data);
 	/* a lancer la fonction scan_fd que si il n'y a pas derreur d'arg comme > ou >> puis rien. 
-	Print le msg d'erreur suivant si le cas au dessus arrive et set code a ERROR_PARS :
+	Print le msg d'erreur suivant si le cas au dessus arrive :
 	bash: syntax error near unexpected token `newline'
 	*/
 	code = is_builting_cmd(data);
